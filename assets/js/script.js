433c77ba6cb828ae6fa71eacecfab98f524e3861
Anas Azizi <anasazizi263@gmail.com>
2024-05-11 13:15:35 +0300

    new feature -sticky navbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,51 +1,60 @@
-const swiperSliderMain = new Swiper(".slider--main", {
-    slidesPerView: 1,
+
+document.addEventListener("DOMContentLoaded", (event) => {
+    const swiperSliderMain = new Swiper(".slider--main", {
+        slidesPerView: 1,
+    });
 });
 
-var swiper = new Swiper(".slider-feedback-wrapper", {
-    slidesPerView: 1,
-    spaceBetween: 30,
-    loop: true,
-    breakpoints: {
-        992: {
-            slidesPerView: 2,
-            spaceBetween: 30,
-        },
-        1400: {
-            slidesPerView: 3,
-            spaceBetween: 30,
-        },
-    }
+document.addEventListener("DOMContentLoaded", (event) => {
+    const swiper = new Swiper(".slider-feedback-wrapper", {
+        slidesPerView: 1,
+        spaceBetween: 30,
+        loop: true,
+        breakpoints: {
+            992: {
+                slidesPerView: 2,
+                spaceBetween: 30,
+            },
+            1400: {
+                slidesPerView: 3,
+                spaceBetween: 30,
+            },
+        }
+    });
 });
 
-const sliderStatistics = new Swiper(".slider--statistsics", {
-    slidesPerView: 2,
-    spaceBetween: 30,
-    loop: true,
-    breakpoints: {
-        768: {
-            slidesPerView: 3,
-            spaceBetween: 30,
-        },
-        1400: {
-            slidesPerView: 5,
-            spaceBetween: 30,
-        },
-    }
+document.addEventListener("DOMContentLoaded", (event) => {
+    const sliderStatistics = new Swiper(".slider--statistsics", {
+        slidesPerView: 2,
+        spaceBetween: 30,
+        loop: true,
+        breakpoints: {
+            768: {
+                slidesPerView: 3,
+                spaceBetween: 30,
+            },
+            1400: {
+                slidesPerView: 5,
+                spaceBetween: 30,
+            },
+        }
+    });
 });
 
-var swiper = new Swiper(".slider--team", {
-    slidesPerView: 1,
-    spaceBetween: 30,
-    loop: true,
-    breakpoints: {
-        768: {
-            slidesPerView: 2,
-            spaceBetween: 30,
-        },
-        1400: {
-            slidesPerView: 3,
-            spaceBetween: 30,
-        },
-    }
+document.addEventListener("DOMContentLoaded", (event) => {
+    const swiper = new Swiper(".slider--team", {
+        slidesPerView: 1,
+        spaceBetween: 30,
+        loop: true,
+        breakpoints: {
+            768: {
+                slidesPerView: 2,
+                spaceBetween: 30,
+            },
+            1400: {
+                slidesPerView: 3,
+                spaceBetween: 30,
+            },
+        }
+    });
 });
